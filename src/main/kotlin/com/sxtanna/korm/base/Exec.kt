6248c0a8ce00fab5feafe18714de21d9fc971d58
Sxtanna <ranald.taylor@gmail.com>
2018-09-24 03:01:31 -0400

    Changed to Exec, and added documentation

--- src/main/kotlin/com/sxtanna/korm/base/Exec.kt
+++ src/main/kotlin/com/sxtanna/korm/base/Exec.kt
@@ -1,7 +1,13 @@
 package com.sxtanna.korm.base
 
+/**
+ * Represents a class that has a block of code that will be "executed"
+ */
 interface Exec<out T : Any> {
 
-    fun eval(): T
+    /**
+     * Execute this class' function
+     */
+    fun exec(): T
 
 }
