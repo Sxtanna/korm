94fdb244529d59c1ca443ec488b95a63ddc134b5
Sxtanna <ranald.taylor@gmail.com>
2018-09-24 03:01:43 -0400

    Added documentation

--- src/main/kotlin/com/sxtanna/korm/base/KormPuller.kt
+++ src/main/kotlin/com/sxtanna/korm/base/KormPuller.kt
@@ -3,18 +3,35 @@ package com.sxtanna.korm.base
 import com.sxtanna.korm.data.KormType
 import com.sxtanna.korm.reader.KormReader
 
+/**
+ * A custom method for pulling an object from korm data
+ *
+ *  - Should properly override [pull] to produce an instance of [T]
+ */
 interface KormPuller<out T : Any> {
 
+    /**
+     * Create an object [T] from the current [reader] using the provided data in [types]
+     */
     fun pull(reader: KormReader.ReaderContext, types: MutableList<KormType>): T?
 
 
+    /**
+     * Convenience method for throwing an [IllegalStateException] with the provided [reason]
+     */
     fun badState(reason: String): Nothing {
         throw IllegalStateException(reason)
     }
 
+    /**
+     * Find a [KormType] with the provided [name], with the option to [remove] it from the given list
+     */
     fun MutableList<KormType>.typeByName(name: String, remove: Boolean = false): KormType? {
         val korm = find { it.key.data.toString() == name } ?: return null
-        if (remove) remove(korm)
+
+        if (remove) {
+            remove(korm)
+        }
 
         return korm
     }
--- src/main/kotlin/com/sxtanna/korm/base/KormPusher.kt
+++ src/main/kotlin/com/sxtanna/korm/base/KormPusher.kt
@@ -2,8 +2,16 @@ package com.sxtanna.korm.base
 
 import com.sxtanna.korm.writer.KormWriter
 
+/**
+ * A custom method for pushing an object to korm data
+ *
+ *  - Should properly override [push] to produce valid korm data
+ */
 interface KormPusher<in T : Any> {
 
-    fun push(data: T?, writer: KormWriter.WriterContext)
+    /**
+     * Create korm data with the given [writer] from the provided [data]
+     */
+    fun push(writer: KormWriter.WriterContext, data: T?)
 
 }
